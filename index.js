38e45d68ba773b68d2bb5b90cfeac105a412ff99
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-04-23 22:08:37 +0530

    updated

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const roomRoute = require("./routes/room");
 const tenantRoute = require("./routes/tenant");
 const paymentRoute = require("./routes/payment");
 // const { cornJob } = require("./utils/utils");
+const path = require("path");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -20,6 +21,24 @@ app.use("/owner/auth", ownerAuthRoute);
 app.use("/room", roomRoute);
 app.use("/tenant", tenantRoute);
 app.use("/payment", paymentRoute);
+
+app.get("/app-version", (req, res) => {
+  const resObj = {
+    latestVersion: "1.0", // Example version
+    mandatory: true, // Example mandatory flag
+    apkUrl: "https://rent-management-backend-three.vercel.app/download-apk", // Example APK URL
+  };
+  res.json(resObj);
+});
+
+app.get("/download-apk", (req, res) => {
+  const apkPath = path.join(__dirname, "apk", "rentManagement.apk"); // replace with your APK file name
+  res.download(apkPath, "rentManagement.apk", (err) => {
+    if (err) {
+      res.status(500).send("Error downloading the file");
+    }
+  });
+});
 
 // // This handler is used for scheduled (cron) tasks on Vercel.
 // // This is our serverless function handler which will be used by Vercel for cron jobs or API requests.
