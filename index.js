27ce6c99792b5992dfd643684e969ecf14d38225
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-04-24 06:59:38 +0530

    apk version change

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/payment", paymentRoute);
 
 app.get("/app-version", (req, res) => {
   const resObj = {
-    latestVersion: "1.0", // Example version
+    latestVersion: "1.0.1", // Example version
     mandatory: true, // Example mandatory flag
     apkUrl: "https://rent-management-backend-three.vercel.app/download-apk", // Example APK URL
   };
